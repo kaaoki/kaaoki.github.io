b9ded50a1f0d04a48429532e7d0433c88cac4b31
kaaoki <69283387+kaaoki@users.noreply.github.com>
2020-08-07 11:43:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 $(function(){
-  
-  $('.signup').click(function(){
-    $('#heart').slideUp(300);
-  });
-  
-  /*
+    
   $('.signup').clickToggle(function () {
     // １回目のクリック
     $('#heart').slideDown(300);
@@ -12,19 +7,19 @@ $(function(){
     // ２回目のクリック
     $('#heart').slideUp(300);
   });
-  */
   
   $.fn.clickToggle = function (a, b) {
-  return this.each(function () {
-    var clicked = false;
-    $(this).on('click', function () {
-      clicked = !clicked;
-      if (clicked) {
-        return a.apply(this, arguments);
-      }
-      return b.apply(this, arguments);
+    return this.each(function () {
+      var clicked = false;
+      $(this).on('click', function () {
+        clicked = !clicked;
+        if (clicked) {
+          return a.apply(this, arguments);
+        }
+        return b.apply(this, arguments);
+      });
     });
-  });
+  };
 
 });
 
