df03d462e45f92838318e0904ee93b60c637c376
kaaoki <69283387+kaaoki@users.noreply.github.com>
2020-08-07 11:07:24 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function(){
+  $('#title').clickToggle(function () {
+    // １回目のクリック
+    $('#heart').slideDown(300);
+  }, function () {
+    // ２回目のクリック
+    $('#heart').slideUp(300);
+  });
+});
+
+$.fn.clickToggle = function (a, b) {
+  return this.each(function () {
+    var clicked = false;
+    $(this).on('click', function () {
+      clicked = !clicked;
+      if (clicked) {
+        return a.apply(this, arguments);
+      }
+      return b.apply(this, arguments);
+    });
+  });
+};
