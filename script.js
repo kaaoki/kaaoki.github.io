46e730b9730596483cb4317ff70877f797048678
kaaoki <69283387+kaaoki@users.noreply.github.com>
2020-08-07 17:28:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 $(function(){
     
+  let $heart = $('#heart');
+  
     $('#title').click(function(){
-      $('#heart').css('color','red');      
-      $('#heart').slideDown(300);
-      $('#heart').slideUp(1000);      
+      $heart.css('color','red').slideDown(300).slideUp(1000);      
     });
  
     $('.signup').click(function(){
-      $('#heart').css('color','#239b76');
-      $('#heart').slideDown(300);
-      $('#heart').slideUp(1000);      
+      $heart.css('color','#239b76').slideDown(300).slideUp(1000);
     });
     
     $('.facebook').click(function(){
-      $('#heart').css('color','#3b5998');
-      $('#heart').slideDown(300);
-      $('#heart').slideUp(1000);      
+      $heart.css('color','#3b5998').slideDown(300).slideUp(1000);
     });
     
     $('.twitter').click(function(){
-      $('#heart').css('color','#55acee');
-      $('#heart').slideDown(300);
-      $('#heart').slideUp(1000);      
+      $heart.css('color','#55acee').slideDown(300).slideUp(1000);
     });    
 
 });
